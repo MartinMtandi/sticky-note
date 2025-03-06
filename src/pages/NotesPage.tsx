38316989f55c7d541202c4c14b1447a76acd1807
MartinMtandi <mtandimartin@gmail.com>
2025-03-06 16:33:34 +0200

    Add feature

--- src/pages/NotesPage.tsx
+++ src/pages/NotesPage.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { Note } from '../utils/types';
 import NoteCard from '../components/NoteCard';
 import { useNotes } from '../services/useNotes';
+import Controls from '../components/Controls';
 
 const NotesPage: FC = () => {
   const { notes, deleteNote  } = useNotes();
@@ -11,6 +12,7 @@ const NotesPage: FC = () => {
       {notes.map((note: Note) => (
         <NoteCard key={note.$id} note={note} onDelete={() => deleteNote(note.$id)} />
       ))}
+      <Controls />
     </React.Fragment>
   )
 }
