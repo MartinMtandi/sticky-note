99f0f13d23965492bf98e74b186c71b6f7d96d1f
MartinMtandi <mtandimartin@gmail.com>
2025-03-14 15:38:44 +0200

    Update Pill Styling

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -1,49 +1,53 @@
-import styled from 'styled-components';
-import { Priority, PRIORITY_COLORS } from '../utils/constants';
-import { TASK_STATUS_COLORS, TASK_STATUS_LABELS } from '../utils/constants';
-import { MouseEvent } from 'react';
-import { lightenHexColor } from '../utils';
+import styled from "styled-components";
+import { Priority, PRIORITY_COLORS } from "../utils/constants";
+import { TASK_STATUS_COLORS, TASK_STATUS_LABELS } from "../utils/constants";
+import { MouseEvent } from "react";
+import { lightenHexColor } from "../utils";
 
 interface PillProps {
-    priority?: Priority;
-    taskStatus?: keyof typeof TASK_STATUS_COLORS;
-    onClick?: (e: MouseEvent) => void;
-    disabled?: boolean;
+  priority?: Priority;
+  taskStatus?: keyof typeof TASK_STATUS_COLORS;
+  onClick?: (e: MouseEvent) => void;
+  disabled?: boolean;
 }
 
-export const Pill = ({ priority, taskStatus, onClick, disabled }: PillProps) => {
-    // Determine the color and label
-    const color = taskStatus 
-        ? TASK_STATUS_COLORS[taskStatus] 
-        : priority 
-            ? PRIORITY_COLORS[priority] 
-            : PRIORITY_COLORS.MEDIUM;
+export const Pill = ({
+  priority,
+  taskStatus,
+  onClick,
+  disabled,
+}: PillProps) => {
+  // Determine the color and label
+  const color = taskStatus
+    ? TASK_STATUS_COLORS[taskStatus]
+    : priority
+    ? PRIORITY_COLORS[priority]
+    : PRIORITY_COLORS.MEDIUM;
 
-    const label = taskStatus 
-        ? TASK_STATUS_LABELS[taskStatus] 
-        : priority || 'MEDIUM';
+  const label = taskStatus
+    ? TASK_STATUS_LABELS[taskStatus]
+    : priority || "MEDIUM";
 
-    const title = taskStatus
-        ? `${label} - Click to cycle`
-        : `${label} Priority - Click to cycle`;
+  const title = taskStatus
+    ? `${label} - Click to cycle`
+    : `${label} Priority - Click to cycle`;
 
-    return (
-        <PillContainer 
-            $color={color}
-            onClick={onClick}
-            title={title}
-            style={{ opacity: disabled ? 0.5 : 1 }}
-            role="button"
-            aria-label={title}
-            data-priority={priority || 'MEDIUM'}
-            data-status={taskStatus || undefined}
-        >
-            {label}
-        </PillContainer>
-    );
+  return (
+    <PillContainer
+      $color={color}
+      $taskStatus={taskStatus} // Explicitly pass taskStatus
+      onClick={onClick}
+      title={title}
+      style={{ opacity: disabled ? 0.5 : 1 }}
+      role="button"
+      aria-label={title}
+    >
+      {label}
+    </PillContainer>
+  );
 };
 
-const PillContainer = styled.div<{ $color: string }>`
+const PillContainer = styled.div<{ $color: string; $taskStatus?: string }>`
     /* Size and shape */
     padding: ${({ theme }) => theme.spacing.xs} ${({ theme }) => theme.spacing.sm};
     border-radius: ${({ theme }) => theme.borderRadius.sm};
@@ -57,8 +61,8 @@ const PillContainer = styled.div<{ $color: string }>`
     
     /* Colors */
     border: 1px solid ${props => props.$color};
-    background-color: ${props => lightenHexColor(props.$color)};
-    color: ${props => props.$color};
+    background-color: ${props => (props.$taskStatus ? props.$color : lightenHexColor(props.$color))};
+    color: ${props => (props.$taskStatus ? 'white' : props.$color)};
     
     /* Interaction */
     cursor: pointer;
@@ -68,8 +72,8 @@ const PillContainer = styled.div<{ $color: string }>`
     /* Hover effect - scale animation */
     &:hover {
         transform: scale(1.05);
-        background-color: ${props => props.$color};
-        color: white;
+        background-color: ${props => (props.$taskStatus ? lightenHexColor(props.$color) : props.$color)};
+        color: ${props => (props.$taskStatus ? props.$color : 'white')};
     }
 
     /* Disabled state */
@@ -77,3 +81,4 @@ const PillContainer = styled.div<{ $color: string }>`
         cursor: not-allowed;
     }
 `;
+
